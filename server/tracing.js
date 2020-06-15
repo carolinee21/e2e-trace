4edd9c63348c435b1cf380ddfffad5d74b51253d
Caroline Evans <caroline.evans@seas.upenn.edu>
2020-06-15 00:21:08 -0700

    Initial OpenTelemetry imports/set-up

--- server/tracing.js
+++ server/tracing.js
@@ -1,17 +1,31 @@
-const traceProvider = require('@opencensus/nodejs');
-const { StackdriverTraceExporter } = require('@opencensus/exporter-stackdriver');
-const exporter = new StackdriverTraceExporter({projectId: "ardent-fusion-279020"});
-const propagation = require('@opencensus/propagation-b3');
+const { NodeTracerProvider } = require("@opentelemetry/node");
+const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
+const { TraceExporter } = require('@google-cloud/opentelemetry-cloud-trace-exporter');
+
+// const exporter = new StackdriverTraceExporter({projectId: "ardent-fusion-279020"});
 
 export default function initTracer () {
 
-    traceProvider.registerExporter(exporter).start({
-        exporter,
-        samplingRate: 1, // Always sample (for testing purposes)
-        propagation: new propagation.B3Format(),
-        logLevel: 1 // show errors
-      });
-    
+  const tracerProvider = new NodeTracerProvider();
+
+    // traceProvider.registerExporter(exporter).start({
+    //     exporter,
+    //     samplingRate: 1, // Always sample (for testing purposes)
+    //     propagation: new propagation.B3Format(),
+    //     logLevel: 1 // show errors
+    //   });
+
+    tracerProvider.addSpanProcessor(
+      new SimpleSpanProcessor(
+        new TraceExporter({
+          // If you are not in a GCP environment, you will need to provide your
+          // service account key here. See the Authentication section in the docs.
+        })
+      )
+    );
+
+    tracerProvider.register();
+
     const tracer = traceProvider.tracer;
     return tracer;
 }
